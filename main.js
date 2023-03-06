415f7b1a70ee215711b53f65cf0cd4a2035f0a60
Fez06 <mattiafezzardi1993@gmail.com>
2023-03-06 16:36:36 +0100

    prima fase di risoluzione

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
 'use strict';
+
+
+
+// -------------------FUNZIONI
+
+//crea numero random
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
+//controllore(non so se servira')
+function controllore(arrayName, valore) {
+    for (let i = 0; i < arrayName.length; i++) {
+
+        //console.log(arrayName[i]);
+        if (valore === arrayName[i]) {
+            console.log('gia esistente');
+            return true
+        } 
+
+    }
+    console.log('diverso');
+    return false
+}
+
+
+//riempi array con valori random unici
+function fillArrayUnicCasual(nValori, valoreMax, arrayName) {
+
+    let i = 0;
+    while (i < nValori) {
+
+        const posValue = randomNumber(1, valoreMax);
+        //console.log('roll');
+        //console.log(posValue);
+        if (controllore(arrayName, posValue) === false) {
+            arrayName.push(posValue);
+            i++;
+        }
+
+    }
+    //console.log(arrayName);
+}
+
+
+// -------------------------MAIN
+
+
+const gruppoNumeri = [];
+
+fillArrayUnicCasual(5, 100, gruppoNumeri);
+console.log(gruppoNumeri);
+
+const visualHtml = document.querySelector('div');
+visualHtml.innerText = gruppoNumeri;
