d7604bc26561f69bd729bc155157b98118e569bc
Fez06 <mattiafezzardi1993@gmail.com>
2023-03-06 19:15:03 +0100

    prompt semplici aggiunti

--- main.js
+++ main.js
@@ -45,9 +45,13 @@ function fillArrayUnicCasual(nValori, valoreMax, arrayName) {
 }
 
 
+function hideNumber() {
+    visualHtml.classList.add('hide');
+}
+
 // -------------------------MAIN
 
-
+//prima fase
 const gruppoNumeri = [];
 
 fillArrayUnicCasual(5, 100, gruppoNumeri);
@@ -55,3 +59,52 @@ console.log(gruppoNumeri);
 
 const visualHtml = document.querySelector('div');
 visualHtml.innerText = gruppoNumeri;
+
+
+
+
+//seconda fase
+const playerNumeri = [];
+setTimeout(hideNumber, 3000);
+setTimeout(promptCompile, 4000);
+
+
+/*function promptCompile() {
+    while (playerNumeri.lenght < gruppoNumeri.length) {
+    Number(prompt('inserisci un valore di quelli mostrati prima'));
+    if (controllore(gruppoNumeri, number) === false){
+        playerNumeri.push(number);
+        i++;
+    }
+}}*/
+
+
+/*for (let i = 0; i < 5; i++) {
+    let number = Number(prompt('inserisci un valore di quelli mostrati prima'));
+
+    //while (playerNumeri.lenght < i) {
+        
+        if (controllore(gruppoNumeri, number) === true){
+            playerNumeri.push(number);
+            //i++;
+        }
+    //}
+    console.log(playerNumeri);
+}*/
+
+
+
+function promptCompile() {
+    for (let i = 0; i < 5; i++) {
+        let number = Number(prompt('inserisci un valore di quelli mostrati prima'));
+    
+        //while (playerNumeri.lenght < i) {
+            
+            if (controllore(gruppoNumeri, number) === true){
+                playerNumeri.push(number);
+                //i++;
+            }
+        //}
+        console.log(playerNumeri);
+    }
+}
